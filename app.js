51ccabdd464e2dae53aa32b3314278e3cb14073c
abhignavuppala01@gmail.com <abhiv053@gmail.com>
2020-10-16 12:59:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+const app=express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname+"/index.html");
+
+});
+
+
+app.post("/",function(req,res)
+{
+    const query=req.body.cityName;
+    const apikey="1ae7d071e071ee96fca748802ad6930d";
+    const units="metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+units+"&appid="+apikey+"";
+    const imageurl="https://openweathermap.org/img/wn/10d@2x.png";
+// https.get(url,function(response){
+//     console.log(response.statusCode);
+// })
+https.get(url,function(response)
+{
+    console.log(response.statusCode);
+
+    response.on("data",function(data){
+        const weatherData=JSON.parse(data);
+        const temp=weatherData.main.temp;
+        const description =weatherData.weather[0].description;
+        const icon=weatherData.weather[0].icon;
+        const imageurl="https://openweathermap.org/img/wn/"+icon+"@2x.png";
+        res.write("<p>The weather is currently "+description+"</p>");
+        res.write("<h1>temperature in "+query+" is"+ temp+"  (in celcius)</h1>");
+        res.write("<img src=" + imageurl+">");
+        res.send();
+    })
+})
+})
+
+app.listen(3000,function()
+{
+    console.log("server is running on port 3000");
+});
